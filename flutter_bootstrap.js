0c3bf26c395d7beac48d7fe6adb2a7209f2b0710
VincentRolongDevelop <vincent.rolong.develop@gmail.com>
2025-06-06 10:51:56 -0500

    new things added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3261060114"
+    serviceWorkerVersion: "1481914139"
   }
 });
